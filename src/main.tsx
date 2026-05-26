69e178d9ddfb147c6d4300f753c5f53c0add6f3a
zoey203 <3597979100@qq.com>
2026-05-26 22:47:12 +0800

    fix: add basename for GitHub Pages routing

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/Lolita">
       <WardrobeProvider>
         <App />
       </WardrobeProvider>
